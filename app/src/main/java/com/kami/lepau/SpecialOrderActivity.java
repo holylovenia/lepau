7a8d008deae421f09d2857ab540a940aec245d03
Rionaldi Chandraseta <rionaldi.chandraseta@gmail.com>
2017-11-09 16:50:08 +0700

    Added layout for special-order-base

--- app/src/main/java/com/kami/lepau/SpecialOrderActivity.java
+++ app/src/main/java/com/kami/lepau/SpecialOrderActivity.java
@@ -45,15 +45,6 @@ public class SpecialOrderActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
     }
 
 
