8e6d1aa3cdad23f07cb94c3b194a356f7e116fd3
holylovenia <holy.lovenia@gmail.com>
2017-11-10 17:21:01 +0700

    Added ShoppingCartActivity (front-end)

---
+++ app/src/main/java/com/kami/lepau/data/ShoppingCartActivity.java
@@ -0,0 +1,15 @@
+package com.kami.lepau.data;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.kami.lepau.R;
+
+public class ShoppingCartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_cart);
+    }
+}
---
+++ app/src/main/java/com/kami/lepau/data/ShoppingCartAdapter.java
@@ -0,0 +1,79 @@
+package com.kami.lepau.data;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.kami.lepau.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Holy on 09-Nov-17.
+ */
+
+public class ShoppingCartAdapter extends RecyclerView.Adapter<ShoppingCartAdapter.ViewHolder> {
+    private ArrayList<OrderItem> orderItems;
+    private Context context;
+
+    public ShoppingCartAdapter(ArrayList<OrderItem> orderItems, Context context) {
+        this.orderItems = orderItems;
+        this.context = context;
+    }
+
+    @Override
+    public ShoppingCartAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.menu_list, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(ShoppingCartAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return orderItems.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        //Member Variables for the TextViews
+        private TextView mTitleText;
+        private TextView mPriceText;
+        private TextView mQuantityText;
+        private TextView mSubtotalText;
+
+        /**
+         * Constructor for the ViewHolder, used in onCreateViewHolder().
+         * @param itemView The rootview of the list_item.xml layout file
+         */
+        ViewHolder(View itemView) {
+            super(itemView);
+
+            //Initialize the views
+            mTitleText = (TextView)itemView.findViewById(R.id.shoppingCartList_menuTitleTextView);
+            mQuantityText = (TextView) itemView.findViewById(R.id.shoppingCartList_menuQuantityTextView);
+            mPriceText = (TextView) itemView.findViewById(R.id.shoppingCartList_menuPriceTextView);
+            mSubtotalText = (TextView) itemView.findViewById(R.id.shoppingCartList_menuSubtotalTextView);
+
+            itemView.setOnClickListener(this);
+        }
+
+        void bindTo(OrderItem currentOrderItem){
+            //Populate the textviews with data
+            mTitleText.setText(currentOrderItem.getItemName());
+            mQuantityText.setText(Integer.toString(currentOrderItem.getQuantity()));
+            mPriceText.setText(Integer.toString(currentOrderItem.getPricePerItem()));
+            mSubtotalText.setText(Integer.toString(currentOrderItem.getTotalPrice()));
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+}
