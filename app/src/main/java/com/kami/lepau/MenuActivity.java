3564b3baee32015ec03e17f5aa346a8c05498e64
holylovenia <holy.lovenia@gmail.com>
2017-11-11 15:15:16 +0700

    Added navigation bar (default) in menu activity

--- app/src/main/java/com/kami/lepau/MenuActivity.java
+++ app/src/main/java/com/kami/lepau/MenuActivity.java
@@ -5,10 +5,14 @@ import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Parcelable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -19,7 +23,7 @@ import com.kami.lepau.data.OrderItemsAdapter;
 
 import java.util.ArrayList;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private final String SAVED_STATE = "savedOrder";
     //Member variables
@@ -45,6 +49,8 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         mOrder = new Order();
 
@@ -171,6 +177,13 @@ public class MenuActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
         if (backPressedOnce) {
             finishAffinity();
             return;
@@ -179,5 +192,24 @@ public class MenuActivity extends AppCompatActivity {
         Toast.makeText(this, "Press back again to leave", Toast.LENGTH_SHORT).show();
 
         handler.postDelayed(runnable, 2000);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_search) {
+            // Handle search action
+        } else if (id == R.id.nav_tracking_order) {
+            //TODO: tracking order tambahin di sini ya (rio)
+        } else if (id == R.id.nav_history) {
+            //TODO: panggil action history (holy)
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
